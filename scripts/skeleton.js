2a6a6e71bca739bcc6369c3864bd2a283921e3d1
Daniel Chopty <daniel.chopty@hotmail.com>
2024-10-27 19:10:57 -0700

    Completed Firebase authentication; changed index.html to main.html,

--- scripts/skeleton.js
+++
@@ -1,8 +0,0 @@
-//---------------------------------------------------
-// This function loads the parts of your skeleton 
-// (navbar, footer, and other things) into html doc. 
-//---------------------------------------------------
-function loadSkeleton(){
-    console.log($('#navbarPlaceholder').load('./text/nav_before_login.html'));
-}
-loadSkeleton();  //invoke the function
