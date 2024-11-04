3116d2d4b116288d7e7604951ebf13bdd2f64314
Owen Wou <19198526+metalmind@users.noreply.github.com>
2024-11-04 08:00:22 -0800

    hardcoded more info into the demo page

--- scripts/task_list.js
+++ scripts/task_list.js
@@ -2,10 +2,12 @@
 // Input param is the String representing the day of the week, aka, the document name
 function readTaskName(task) {
     db.collection("tasks").doc(task)                                                         //name of the collection and documents should matach excatly with what you have in Firestore
-        .onSnapshot(dayDoc => {                                                              //arrow notation
-            console.log("current document data: " + dayDoc.data());                          //.data() returns data object
-            document.getElementById("task-name-goes-here").innerHTML = dayDoc.data().name;      //using javascript to display the data on the right place
-
+        .onSnapshot(taskDoc => {                                                              //arrow notation
+            console.log("current document data: " + taskDoc.data());                          //.data() returns data object
+            document.getElementById("task-name-goes-here").innerHTML = taskDoc.data().name;      //using javascript to display the data on the right place
+            document.getElementById("due-date-goes-here").innerHTML = taskDoc.data().due; 
+            document.getElementById("descripition-goes-here").innerHTML = taskDoc.data().description; 
+            document.getElementById("course-goes-here").innerHTML = taskDoc.data().course; 
             //Here are other ways to access key-value data fields
             //$('#quote-goes-here').text(dayDoc.data().quote);         //using jquery object dot notation
             //$("#quote-goes-here").text(dayDoc.data()["quote"]);      //using json object indexing
