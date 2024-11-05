7a677a93a4be5b8626cb1b9b538c9eb9f734003f
Owen Wou <19198526+metalmind@users.noreply.github.com>
2024-11-04 16:27:09 -0800

    front end connected to back end

--- scripts/task_list.js
+++ scripts/task_list.js
@@ -19,26 +19,29 @@ function readTaskName(task) {
     }
  readTaskName("task1");        //calling the function
 
- //------------------------------------------------------------------------------
+   //------------------------------------------------------------------------------
 // Input parameter is a string representing the collection we are reading from
 //------------------------------------------------------------------------------
 function displayCardsDynamically(collection) {
-    let cardTemplate = document.getElementById("taskCardTemplate"); // Retrieve the HTML element with the ID "hikeCardTemplate" and store it in the cardTemplate variable. 
-
+    let cardTemplate = document.getElementById("hikeCardTemplate"); // Retrieve the HTML element with the ID "hikeCardTemplate" and store it in the cardTemplate variable. 
+    
     db.collection(collection).get()   //the collection called "hikes"
-        .then(allTasks=> {
+        .then(allHikes=> {
             //var i = 1;  //Optional: if you want to have a unique ID for each hike
-            allTasks.forEach(doc => { //iterate thru each doc
+            allHikes.forEach(doc => { //iterate thru each doc
                 var title = doc.data().name;       // get value of the "name" key
-                var details = doc.data().description;  // get value of the "details" key
-				var taskCode = doc.data().code;    //get unique ID to each hike to be used for fetching right image
-                var taskDue = doc.data().due; //gets the length field
+                var details = doc.data().details;  // get value of the "details" key
+								var hikeCode = doc.data().code;    //get unique ID to each hike to be used for fetching right image
+                var length = doc.data().hikeLength; //gets the length field
+                var docID = doc.id;
                 let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
-
+                
                 //update title and text and image
-                newcard.querySelector('.task-name').innerHTML = title;
-                newcard.querySelector('.task-due').innerHTML = taskDue;
-                newcard.querySelector('.task-description').innerHTML = details;
+                newcard.querySelector('.card-title').innerHTML = title;
+                newcard.querySelector('.card-length').innerHTML = length +"km";
+                newcard.querySelector('.card-text').innerHTML = details;
+                // newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
+                // newcard.querySelector('a').href = "eachHike.html?docID="+docID;
 
                 //Optional: give unique ids to all elements for future use
                 // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
