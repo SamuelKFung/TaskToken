5ab9ad8c00c71e7e4e06040adc1bf6c5023f9552
Owen Wou <19198526+metalmind@users.noreply.github.com>
2024-11-09 11:48:27 -0800

    connected to back end using Carly's B01a and B01b, still needs to be customed for our use

--- public/js/task_list.js
+++ public/js/task_list.js
@@ -1,4 +1,3 @@
-console.log("task_list.js called")
 // Dynamically change modals based on which button is pressed 
 const exampleModal = document.getElementById('exampleModal');
 if (exampleModal) {
@@ -28,9 +27,49 @@ if (exampleModal) {
         modalTitleContent.textContent = recipient;
     })
 }
-displayCardsDynamically("tasks");  //input param is the name of the collection
 
-document.querySelector("#addbutton").addEventListener("click", function (e) {
-    console.log("clicked");
-    
-});
+//-------------------------------------------------
+// this function shows finds out who is logged in,
+// reads the "myposts" field (an array) for that user, 
+// reads the details for each item in the array
+// and displays a card for each item. 
+//------------------------------------------------
+function showMyTasks() {
+      firebase.auth().onAuthStateChanged(user => {
+            console.log("user is: " + user.uid);
+            db.collection("users").doc(user.uid)
+                    .get()
+                    .then(doc => {
+                        myposts = doc.data().myposts; //get array of my posts
+                        console.log(myposts);
+                        myposts.forEach(item => {
+                            db.collection("posts")
+                                .doc(item)
+                                .get()
+                                .then(doc => {
+                                    displayMytaskCard(doc);
+                                })
+                        })
+                    })
+      })
+}
+showMyTasks();
+
+//------------------------------------------------------------
+// this function displays ONE card, with information
+// from the post document extracted (name, description, image)
+//------------------------------------------------------------
+function displayMytaskCard(doc) {
+            var title = doc.data().name; // get value of the "name" key
+            var desc = doc.data().description; //gets the length field
+            var image = doc.data().image; //the field that contains the URL 
+
+            //clone the new card
+            let newcard = document.getElementById("taskCardTemplate").content.cloneNode(true);
+            //populate with title, image
+            newcard.querySelector('.card-title').innerHTML = title;
+            //newcard.querySelector('.card-image').src = image;
+            //newcard.querySelector('.card-description').innerHTML = desc;
+            //append to the posts
+            document.getElementById("mytasks-go-here").append(newcard);
+}
