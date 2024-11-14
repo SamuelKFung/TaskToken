85b7a71af3e323c20a01eabc9bd03138e6b20f50
Owen Wou <19198526+metalmind@users.noreply.github.com>
2024-11-14 10:20:52 -0800

    Updated to display tasks from database

--- public/js/task_list.js
+++ public/js/task_list.js
@@ -53,98 +53,29 @@ function writeTasks() {
         }
     });
 }
-/*
+
 function getTasks(){
     firebase.auth().onAuthStateChanged(function(user) {
         if (user){
             console.log(user.uid)
             db.collection("users").doc(user.uid)
-            .collection("") //subcollection
+            .collection("tasks") //subcollection
             .get()
             .then(doclist=>{
                 doclist.forEach(doc=>{
-                    console.log (doc.data());   //unpack and see all the attributes
-                    //document.getElementById("tasks-go-here").innerHTML += "<p> " + doc.data().title + " </p>"
-                }
-                )
+                    currentTask = doc;
+                    console.log (currentTask);   //unpack and see all the attributes
+                    displayMytaskCard(currentTask);
+                })
             })
-        } else{
+        } else {
             console.log("No user logged in");
         }
     })
 }
 getTasks();
-=======
 
-function writeReview() {
-    let taskName = document.getElementById("title").value;
-    let taskDate = document.getElementById("date").value;
-    let taskDescription = document.getElementById("description").value;
-    console.log(taskName, taskDate, taskDescription);
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            db.collection("users").doc(user.uid).collection("assignments").add({
-                title: taskName,
-                
-            })
-            
-        } else {
-            console.log("fail");
-        }
-    });
-
-}
-/*
-        // Get the document for the current user.
-        db.collection("reviews").add({
-            hikeDocID: hikeDocID,
-            userID: userID,
-            title: hikeTitle,
-            level: hikeLevel,
-            season: hikeSeason,
-            description: hikeDescription,
-            flooded: hikeFlooded,
-            scrambled: hikeScrambled,
-            rating: hikeRating, // Include the rating in the review
-            timestamp: firebase.firestore.FieldValue.serverTimestamp()
-        }).then(() => {
-            window.location.href = "thanks.html"; // Redirect to the thanks page
-        });
-    } else {
-        console.log("No user is signed in");
-        window.location.href = 'review.html';
-    }
-}
-
-/*
-//-------------------------------------------------
-// this function shows finds out who is logged in,
-// reads the "myposts" field (an array) for that user, 
-// reads the details for each item in the array
-// and displays a card for each item. 
-//------------------------------------------------
-function showMyTasks() {
-      firebase.auth().onAuthStateChanged(user => {
-            console.log("user is: " + user.uid);
-            db.collection("users").doc(user.uid)
-                    .get()
-                    .then(doc => {
-                        myposts = doc.data().posts; //get array of my posts
-                        console.log(myposts);
-                        myposts.forEach(item => {
-                            console.log("item is: " + item)
-                            db.collection("posts")
-                                .doc(item)
-                                .get()
-                                .then(doc => {
-                                    console.log("this is the doc = " + doc.data().name)
-                                    displayMytaskCard(doc);
-                                })
-                        })
-                    })
-      })
-}
-showMyTasks();
+  
 
 //------------------------------------------------------------
 // this function displays ONE card, with information
@@ -153,7 +84,7 @@ showMyTasks();
 function displayMytaskCard(doc) {
             var name = doc.data().name; // get value of the "name" key
             var desc = doc.data().description; 
-            var due = doc.data().date; 
+            var due = doc.data().duedate; 
             //clone the new card
             let newcard = document.getElementById("taskCardTemplate").content.cloneNode(true);
             //populate with title, image
@@ -164,34 +95,6 @@ function displayMytaskCard(doc) {
             //append to the posts
             document.getElementById("mytasks-go-here").append(newcard);
 }
-/*
-function savePost() {
-    alert ("SAVE POST is triggered");
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            // User is signed in.
-            // Do something for the user here. 
-            var name = document.getElementById("name").value;
-            var date = document.getElementById("date").value;
-            var desc = document.getElementById("description").value;
-            db.collection("posts").add({
-                owner: user.uid,
-                name: name,
-                date: date,
-                description: desc,
-                last_updated: firebase.firestore.FieldValue
-                    .serverTimestamp() //current system time
-            }).then(doc => {
-                console.log("1. Post document added!");
-                console.log(doc.id);
-                savePostIDforUser(doc.id);
-            })
-        } else {
-            // No user is signed in.
-                          console.log("Error, no user signed in");
-        }
-    });
-}   
 
 //--------------------------------------------
 //saves the post ID for the user, in an array
@@ -212,4 +115,4 @@ function savePostIDforUser(postDocID) {
                 console.error("Error writing document: ", error);
            });
     })
-}*/
+}
