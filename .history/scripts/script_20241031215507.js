e6b0c59e48f0bddd9982bfad9babfebd424e67f4
Daniel Chopty <daniel.chopty@hotmail.com>
2024-11-21 10:22:24 -0800

    deleted history

--- .history/scripts/script_20241031215507.js
+++
@@ -1,9 +0,0 @@
-//------------------------------------------------
-// Call this function when the "logout" button is clicked
-//-------------------------------------------------
-function logout() {
-    firebase.auth().signOut().then(() => {
-        console.log("logging out user");
-      }).catch((error) => {
-        // An error happened.
-      }); }
